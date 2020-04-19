235cf49145c347864931c5ea43d14b6198251b59
caio-vinicius <sf.caio92@gmail.com>
2020-04-19 12:58:56 -0300

    fix memory leaks

--- get_next_line.c
+++ get_next_line.c
@@ -14,44 +14,49 @@
 
 int			get_next_line(int fd, char **line)
 {
-    int				ret;
-	int				pos;
-	char			*aux;
-    char			*buffer;
-	static char		*tmp;
-	//char			*tmp2;
+	int		ret;
+	int		pos;
+	char		*aux;
+    	char		*buffer;
+	static char	*tmp;
+	char		*tmp2;
 	
 	aux = NULL;
 	if (!(buffer = (char*)malloc((BUFFER_SIZE + 1) * sizeof(char*))))
 		return (-1);
 	if (line == NULL || BUFFER_SIZE <= 0 || fd < 0)
 		return (-1);
-    while ((ret = read(fd, buffer, BUFFER_SIZE)) > 0)
+    	while ((ret = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		buffer[ret] = '\0';
-		//tmp2 = aux;
+		tmp2 = aux;
 		aux = ft_strjoin(aux, buffer);
-		//(tmp2) ? free(tmp2) : 0;
+		free(buffer);
+		buffer = NULL;
+		free(tmp2);
 		if ((ft_strchr_pos(aux, '\n') >= 0))
 			break ;
 	}
+	if (buffer)
+		free(buffer);
 	if (ret < 0)
 		return (-1);
-	//(tmp2) ? free(tmp2) : 0;
+	tmp2 = aux;
 	aux = ft_strjoin(tmp, aux);
-	//(tmp) ? free(tmp) : 0;
+	free(tmp2);
+	free(tmp);
 	pos = ft_strchr_pos(aux, '\n');
 	if (pos >= 0)
 	{
 		*line = ft_substr(aux, 0, pos);
 		tmp = ft_substr(aux, pos + 1, ft_strlen(aux));
 		free(aux);
-		//aux = NULL;
 		return (1);
 	}
 	else
 	{
 		*line = ft_substr(aux, 0, ft_strlen(aux));
+		free(aux);
 		return (0);
 	}
 	return (-1);
